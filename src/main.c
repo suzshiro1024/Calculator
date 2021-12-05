266d51b35caf3651e3d438313a4e17342898e538
Tamamo-Dictus <tix131423113210@gmail.com>
2021-12-06 00:22:05 +0900

    add main.c

---
+++ src/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "stack.h"
+#include "parse.h"
+#include "calc.h"
+
+#define STR_LENGTH 128
+
+int main(void){
+    char str[STR_LENGTH];
+    FILE* fp;
+
+    fp = fopen("calc.txt","r");
+
+    if(fp == NULL){
+        printf(" > ");
+        scanf("%s",str);
+    }else{
+        fscanf(fp,"%s",str);
+    }
+
+    fclose(fp);
+
+    char* buffer = parse(str, sizeof(str)/sizeof(char*));
+
+    //逆ポーランド記法になった文字列バッファの長さを取得
+    int length = strlen(buffer);
+
+    //計算させる
+    int result = calc(buffer, length);
+
+    fp = fopen("calc.txt","a");
+
+    if(fp==NULL){
+        puts("ERROR File Not Found");
+        printf("result = %d",result);
+    }else{
+        fprintf(fp,"=%d\n",result);
+    }
+}
