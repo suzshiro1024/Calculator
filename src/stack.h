913357d30fa81706358f620fc0bd3ac2f614863e
Tamamo-Dictus <tix131423113210@gmail.com>
2021-12-20 00:59:57 +0900

    [header] clearly state the void

--- src/stack.h
+++ src/stack.h
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 //ノード構造体の定義
-typedef struct node{
+typedef struct node
+{
     char data;              //ノードの中身
-    struct node* next_node; //次のノードへのポインタ
+    struct node *next_node; //次のノードへのポインタ
 } node;
 
-node* sp;                   //スタックのtopを指すポインタ(先頭のノードを指す)
+node *sp; //スタックのtopを指すポインタ(先頭のノードを指す)
 
 //関数のプロトタイプ宣言
-bool is_empty();
+bool is_empty(void);
 void push(char value);
-char pop();
-char peek();
-int stack_size();
+char pop(void);
+char peek(void);
+int stack_size(void);
